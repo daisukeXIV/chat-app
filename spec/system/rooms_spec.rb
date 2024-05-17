c65226b3cec1e578666cd992320f93f38b2f5c29
daisukeXIV <daisuke1998gamer@gmail.com>
2024-05-17 10:24:23 +0900

    総合テスト実装（修正）

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -13,10 +13,11 @@ RSpec.describe "Rooms", type: :system do
     click_on(@room_user.room.name)
 
     # メッセージ情報を5つDBに追加する
-    FactoryBot.create_list(:message,5,room_id:@room_user.room.id)
+    FactoryBot.create_list(:message, 5, room_id: @room_user.room.id, user_id: @room_user.user.id)
     # 「チャットを終了する」ボタンをクリックすることで、作成した5つのメッセージが削除されていることを確認する
     expect{
-      click_on("チャットを終了する")
+      # click_on("チャットを終了する")
+      find_link('チャットを終了する',  href: room_path(@room_user.room)).click
       sleep 1
     }.to change{Message.count}.by(-5)
     # トップページに遷移していることを確認する
